cd87a84c9e39347d466a6c8fed5b00f97e9afde7
Débora Abril Badosa <debora.badosa@gmail.com>
2023-04-14 22:02:34 -0300

    arreglando persona

--- src/app/model/persona.model.ts
+++ src/app/model/persona.model.ts
@@ -3,11 +3,13 @@ export class persona{
     nombre: string;
     apellido: string;
     img: string;
+    descripcion: string;
 
-    constructor(nombre:string,apellido:string, img:string){
+    constructor(nombre:string,apellido:string, img:string, descripcion: string){
         this.nombre = nombre;
         this.apellido = apellido;
         this.img = img;
+        this.descripcion = descripcion;
     }
 
 }
--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -8,11 +8,16 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class PersonaService {
-  URL = environment.URL +'personas/';
-
-  constructor(private http: HttpClient) { }
-
-  public getPersona(): Observable<persona>{  
-    return this.http.get<persona>(this.URL+ 'traer/perfil');
+  URL = environment.URL + 'personas/';
+  constructor(private httpClient: HttpClient) {
+  }
+  public lista(): Observable<persona[]>{
+    return this.httpClient.get<persona[]>(this.URL+'lista');
+  }
+  public detail(id:number): Observable<persona>{
+    return this.httpClient.get<persona>(this.URL+`detail/${id}`);
+  }
+  public update(id:number, Persona: persona): Observable<any>{
+    return this.httpClient.put<any>(this.URL+`update/${id}`, Persona);
   }
 }
